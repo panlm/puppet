8518aeccb824168cdb77373d600868240500c546
stevenpan <stevenpan@2e5cfa46-41e0-4c30-b113-11416295e233>
2012-03-06 10:12:15 +0000

    minor edit
    git-svn-id: https://172.29.254.10:3000/svn/projects/puppet@317 2e5cfa46-41e0-4c30-b113-11416295e233

--- client.sh
+++ client.sh
@@ -1,10 +1,10 @@
-rpm -Uhv http://apt.sw.be/redhat/el5/en/i386/rpmforge/RPMS/rpmforge-release-0.3.6-1.el5.rf.i386.rpm
+#rpm -Uhv http://apt.sw.be/redhat/el5/en/i386/rpmforge/RPMS/rpmforge-release-0.3.6-1.el5.rf.i386.rpm
 yes y |yum install ruby
 
 mkdir -p /pub/puppet
 cd /pub/puppet
-wget http://172.29.255.199/facter-1.6.6.tar.gz
-wget http://172.29.255.199/puppet-2.6.14.tar.gz
+wget http://172.30.254.100/facter-1.6.6.tar.gz
+wget http://172.30.254.100/puppet-2.6.14.tar.gz
 tar xf facter-1.6.6.tar.gz
 cd facter-1.6.6
 ruby install.rb
@@ -14,9 +14,9 @@ cd puppet-2.6.14
 ruby install.rb
 cd conf/redhat
 
-cp puppet.conf /etc/puppet/
-cp client.init /etc/init.d/puppet
-cp client.sysconfig /etc/sysconfig/puppet
+/bin/cp puppet.conf /etc/puppet/
+/bin/cp client.init /etc/init.d/puppet
+/bin/cp client.sysconfig /etc/sysconfig/puppet
 chmod 755 /etc/init.d/puppet
 
 useradd puppet
@@ -27,5 +27,8 @@ echo "send dhcp-client-identifier \"`hostname`\";" > /etc/dhclient-eth0.conf
 
 sed -i "/^yinji\.com\.cn/d" /etc/hosts
 sed -i "/^search/d" /etc/resolv.conf
-echo "    server = testvm-0" >> /etc/puppet/puppet.conf
+echo "    server = vm0" >> /etc/puppet/puppet.conf
 puppet agent --test
+service puppet start
+chkconfig puppet on
+
