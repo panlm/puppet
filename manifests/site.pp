60d8b091ffb6f112e5ea0cc8b3ebdc436ebcd48e
stevenpan <stevenpan@2e5cfa46-41e0-4c30-b113-11416295e233>
2012-03-15 10:09:08 +0000

    git-svn-id: https://172.29.254.10:3000/svn/projects/puppet@360 2e5cfa46-41e0-4c30-b113-11416295e233

--- manifests/site.pp
+++ manifests/site.pp
@@ -17,6 +17,7 @@ class base_node_class {
     include http
     include sysctl
     include nginx_compile
+    include siege
 }
 
 # tell puppet on which client to run the class
@@ -33,6 +34,7 @@ node vm1 {
     include http
     include sysctl
     include nginx_compile
+    include siege
 }
 node vm2 { include base_node_class }
 node vm3 { include base_node_class }
@@ -63,5 +65,35 @@ node vm27 { include base_node_class }
 node vm28 { include base_node_class }
 node vm29 { include base_node_class }
 node vm30 { include base_node_class }
+node vm31 { include base_node_class }
+node vm32 { include base_node_class }
+node vm33 { include base_node_class }
+node vm34 { include base_node_class }
+node vm35 { include base_node_class }
+node vm36 { include base_node_class }
+node vm37 { include base_node_class }
+node vm38 { include base_node_class }
+node vm39 { include base_node_class }
+node vm40 { include base_node_class }
+node vm41 { include base_node_class }
+node vm42 { include base_node_class }
+node vm43 { include base_node_class }
+node vm44 { include base_node_class }
+node vm45 { include base_node_class }
+node vm46 { include base_node_class }
+node vm47 { include base_node_class }
+node vm48 { include base_node_class }
+node vm49 { include base_node_class }
+node vm50 { include base_node_class }
+node vm51 { include base_node_class }
+node vm52 { include base_node_class }
+node vm53 { include base_node_class }
+node vm54 { include base_node_class }
+node vm55 { include base_node_class }
+node vm56 { include base_node_class }
+node vm57 { include base_node_class }
+node vm58 { include base_node_class }
+node vm59 { include base_node_class }
+node vm60 { include base_node_class }
 
 #import "modules.pp"
