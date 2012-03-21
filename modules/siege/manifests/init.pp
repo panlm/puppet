0a88961f88077f95565f3bd70b391abdcdb5ec77
stevenpan <stevenpan@2e5cfa46-41e0-4c30-b113-11416295e233>
2012-03-21 05:30:50 +0000

    git-svn-id: https://172.29.254.10:3000/svn/projects/puppet@364 2e5cfa46-41e0-4c30-b113-11416295e233

--- modules/siege/manifests/init.pp
+++ modules/siege/manifests/init.pp
@@ -1,8 +1,35 @@
 class siege {
-    file {
-        "/usr/local/bin/run_siege.sh":
-        source => "puppet://$fileserver/siege/run_siege.sh",
-        mode => 0755;
+    if $hostname =~ /^(vm[0-9]|vm1[0-9])$/ {
+        file {
+            "/usr/local/bin/run_si.sh":
+            source => "puppet://$fileserver/siege/run_si.sh",
+            alias => 'run_si.sh',
+            mode => 0755;
+        }
+        file {
+            "/usr/local/bin/run_si.conf":
+            source => "puppet://$fileserver/siege/run_si.conf",
+            alias => 'run_si.conf',
+            mode => 0644;
+        }
+        cron {
+            run_si_10mins:
+            ensure  => present,
+            require => File['run_si.sh','run_si.conf'],
+            command => "/usr/local/bin/run_si.sh >>/usr/local/bin/run_si.sh.log 2>&1",
+            user => root,
+            minute => '*/10';
+        }
+    } elsif $hostname == 'vm11' {
+        cron {
+            run_si_10mins:
+            ensure  => absent;
+        }
+    } else {
+        cron {
+            run_si_10mins:
+            ensure  => absent;
+        }
     }
 }
 
